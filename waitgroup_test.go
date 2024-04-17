dc0b91a338f93373273d435bfc5d67e3c599882c
Aditya Prasetyo <adty404@gmail.com>
2024-04-17 15:37:26 +0700

    sync.WaitGroup

---
+++ waitgroup_test.go
@@ -0,0 +1,27 @@
+package belajar_golang_goroutines
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func RunAsynchronous(group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+
+	println("Run Asynchronous")
+	time.Sleep(1 * time.Second)
+}
+
+func TestWaitGroup(t *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go RunAsynchronous(group)
+	}
+
+	group.Wait()
+	println("Finish")
+}
